377b82e69a2aab9540dea0f7df4e0dfeb5cc44db
KhanumRabiah <khanumrabiah@gmail.com>
2025-08-18 12:27:54 +0500

    Modified conf.py in dcx.py to our liking

--- rstdoc/dcx.py
+++ rstdoc/dcx.py
@@ -4256,15 +4256,15 @@ example_rest_tree = r'''
          halt_level: severe
          report_level: error
        conf.py
-         project = 'sample'
-         author = project+' Project Team'
+         project = 'RstDoc Project'
+         author = project+' Xgrid Team'
          copyright = '2025, '+author
          version = '1.0'
          release = '1.0.0'
          try:
-             import sphinx_bootstrap_theme
-             html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-             html_theme = 'bootstrap'
+             #import sphinx_bootstrap_theme
+             #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+             html_theme = 'sphinx_rtd_theme'
          except:
              pass
          #these are enforced by rstdoc, but keep them for sphinx-build
@@ -4272,13 +4272,13 @@ example_rest_tree = r'''
          smartquotes = 0
          source_suffix = '.rst'
          templates_path = []
-         language = None
-         highlight_language = "none"
+         language = 'en'
+         #highlight_language = "none"
          default_role = 'math'
          pygments_style = 'sphinx'
          exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
          master_doc = 'index'
-         html_extra_path=['doc/_traceability_file.svg'] #relative to conf.py
+         #html_extra_path=['doc/_traceability_file.svg'] #relative to conf.py
          import os
          on_rtd = os.environ.get('READTHEDOCS') == 'True'
          if not on_rtd:
